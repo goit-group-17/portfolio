3c039d738aff46959361f1df77d077cfbdff4759
SerbynS <serbin.sasha1@gmail.com>
2025-04-04 00:49:27 +0300

    ver 0.2

--- src/js/render-reviews.js
+++ src/js/render-reviews.js
@@ -2,7 +2,7 @@ export function renderGalleryMarkup(reviews) {
   return reviews
     .map(
       ({ author, avatar_url, review }) => `
-        <li class="reviews-item">
+        <li class="reviews-item swiper-slide">
             <img class="reviews-photo" src="${avatar_url}" alt="${author}" width="48" height="48">
             <h3 class="reviews-name">${author}</h3>
             <p class="reviews-content">${review}</p>
--- src/main.js
+++ src/main.js
@@ -7,3 +7,32 @@ const ul = document.querySelector('.reviews-list');
 updateGallery(ul, renderGalleryMarkup(response));
 
 console.log(response);
+
+import Swiper from 'swiper/bundle';
+
+import 'swiper/css';
+// import 'swiper/css/navigation';
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 4,
+  spaceBetween: 16,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    enabled: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      slidesSizesGrid: 343,
+    },
+    768: {
+      slidesPerView: 2,
+      slidesSizesGrid: 344,
+    },
+    1440: {
+      slidesPerView: 4,
+      slidesSizesGrid: 332,
+    },
+  },
+});
