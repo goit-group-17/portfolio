525515ac070f24c814b3f005f4cc84977777f6fc
k.anisimov <k.anisimov@epicentrk.ua>
2025-04-05 16:25:22 +0300

    About me mobile
    About Me Mobile

---
+++ src/js/about-me.js
@@ -0,0 +1,30 @@
+import Accordion from "accordion-js";
+import Swiper from 'swiper/bundle';
+
+export default function InitAboutMe() {
+  new Accordion(".about-me-ac-container", {
+    openOnInit: [0],
+    elementClass: "about-me-ac-item",
+    panelClass: "about-me-ac-panel",
+
+  });
+
+  new Swiper('.about-me-swiper-container', {
+    keyboard: { enabled: true },
+    loop: true,
+    navigation: {
+      nextEl: '.about-me-swiper-button-next',
+      enabled: true,
+
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 2,
+        slidesSizesGrid: 343,
+      },
+    },
+  });
+
+  document.querySelector('.about-me-ac-container').style.display = 'block';
+  document.querySelector('.about-me-swiper').style.display = 'flex';
+}
