46bbf43e03588654252890f603ec613011445ab0
tetiana_hushnovska <thushnyovska@gmail.com>
2025-04-06 20:21:59 +0100

    rewrite jss file

--- src/faq-js/faq.js
+++ src/faq-js/faq.js
@@ -1,23 +1,13 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
-const faqItems = document.querySelectorAll('.faq-ac-item');
-const section = document.querySelector('.section-faq');
-
-faqItems.forEach(item => {
-        const faqTrigger = item.querySelector('.faq-ac-trigger');
-        const faqPanel = item.querySelector('.faq-ac-panel');
-
-    faqTrigger.addEventListener('click', () => {
-        const isOpen = item.classList.contains('is-open');
-
-        item.classList.toggle('is-open');
-
-        if (isOpen) {
-            faqPanel.style.maxHeight = null;
-        }
-        else {
-            faqPanel.style.maxHeight = panel.scrollHeight + 'px';
-        }
-    })
-})
+document.addEventListener('DOMContentLoaded', function () {
+  new Accordion('.js-accordion', {
+    duration: 400,
+    showMultiple: true,
+    elementClass: 'faq-ac-item',
+    triggerClass: 'faq-ac-header',
+    panelClass: 'faq-ac-panel',
+    activeClass: 'is-active'
+  });
+});
