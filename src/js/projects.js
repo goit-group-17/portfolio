18b09c7eb25a6179c2e3f17587e2283fde72ef7b
Tania Dumych <tanja11.malvchko@gmail.com>
2025-04-07 19:48:47 +0200

    commit change

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,12 +1,12 @@
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
-
 document.addEventListener('DOMContentLoaded', () => {
+
     const swiper = new Swiper('.swiper-container', {
         loop: true,
         slidesPerView: 1,
-        spaceBetween: 10,
+        spaceBetween: 10, 
         navigation: {
             nextEl: '.swiper-btn-next',
             prevEl: '.swiper-btn-prev',
@@ -23,11 +23,32 @@ document.addEventListener('DOMContentLoaded', () => {
                         slide.classList.add('hidden');
                     }
                 });
+                updateButtonStates(this);
             },
         },
     });
 
-   const btnSend = document.querySelector('.btn-send');
+   
+    function updateButtonStates(swiper) {
+        const prevButton = document.querySelector('.swiper-btn-prev');
+        const nextButton = document.querySelector('.swiper-btn-next');
+
+        
+        if (swiper.activeIndex === 0) {
+            prevButton.disabled = true;  
+        } else {
+            prevButton.disabled = false; 
+        }
+
+       
+        if (swiper.activeIndex === swiper.slides.length - 1) {
+            nextButton.disabled = true;
+        } else {
+            nextButton.disabled = false; 
+        }
+    }
+
+    const btnSend = document.querySelector('.btn-send');
     if (btnSend) {
         btnSend.addEventListener('click', function () {
             window.open('https://github.com/front-band-17/project-wr23-45', '_blank');
@@ -35,5 +56,5 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         console.error('.btn-send not found!');
     }
-    });
+});
 
